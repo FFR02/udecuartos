551b01a6086f89cbe05d91209eb651c7442ff19c
Kevin Cubillos <kevinlexr@gmail.com>
2021-09-22 14:16:46 -0500

    Se creo el modelo base

---
+++ backend/Models/Hospedaje.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Hospedaje
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Tipo { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string[] Servicios { get; set; }
+    }
+}
